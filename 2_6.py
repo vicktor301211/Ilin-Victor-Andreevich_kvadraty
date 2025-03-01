7ec386b4b88fbdb9ed6a0cac4b8be37919672052
vicktor301211 <iva301211@gmail.com>
2025-03-01 09:57:28 +0300

    Репозиторий на проверку

---
+++ 2_6.py
@@ -0,0 +1,70 @@
+from pygame.constants import *
+
+from all_colors import*
+from random import choice
+import pygame
+pygame.init()
+size = (1280, 720)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Моя игра")
+BACKGROUND = (255, 255, 255)
+screen.fill(BACKGROUND)
+
+x, y = 0, 0
+rect_size = 200
+colors = COLORS
+rects = []
+
+for i in range(1 ,20):
+    rect = pygame.Rect(x, y, rect_size-10*i, rect_size-10*i)
+    rect.center = (screen.get_width()//2, screen.get_height()/2)
+    rects.append(rect)
+
+
+speed = 5
+
+FPS = 60
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    # Обработка событий
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    keys = pygame.key.get_pressed()
+
+
+
+
+
+
+
+
+
+    #Основная логика
+    # if x < 0:
+    #     x = 0
+    #     color = choice(COLORS)
+    #     continue
+    # if x > 1280 - width:
+    #     x = 1280 - width
+    #     color = choice(COLORS)
+    #     continue
+    # if y < 0:
+    #     y = 0
+    #     color = choice(COLORS)
+    #     continue
+    # if y > 720 - height:
+    #     y = 720 - height
+    #     color = choice(COLORS)
+    #     continue
+
+    #Отрисовка объектов
+    for rect in rects:
+        pygame.draw.rect(screen, choice(colors), rect, 10)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+pygame.quit()
